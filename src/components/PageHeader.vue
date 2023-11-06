0affb8b65111569a82d5a4aa6d0c0672ccf1da42
BugaPunks <punksbuga@gmail.com>
2023-11-06 10:22:09 -0400

    [ADD]primer commit

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="text-center my-24">
+        <h1 class="text-5xl font-bold tracking-widest text-sky-700">Atlas App</h1>
+        <h6 class="mt-4 text-xl">Una app simple de paises</h6>
+    </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            // Fuente para títulos
+            quicksand: ["Quicksand", "sans-serif"],
+        },
+        extend: {
+            colors: {
+                clifford: '#da373d',
+            }
+        }
+    }
+}
