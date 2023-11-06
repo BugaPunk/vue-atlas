765374c01eee91f61190094a258b9b9d9e7ec1e7
BugaPunks <punksbuga@gmail.com>
2023-11-06 10:42:33 -0400

    mostrando paises

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <script setup>
-import PageHeader from './components/PageHeader.vue';
+import { onMounted, ref } from "vue";
+import PageHeader from "./components/PageHeader.vue";
+import axiosClient from "./utils/axios";
+
+const countries = ref([]);
+
+const fetchCountries = async () => {
+    try {
+        const { data } = await axiosClient.get("/all");
+        countries.value = data;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+onMounted(() => {
+    fetchCountries();
+});
 </script>
 
 <template>
     <PageHeader />
+    <div v-for="country in countries">
+        {{ country.name.common }}
+    </div>
 </template>
