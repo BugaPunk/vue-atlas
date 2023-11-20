a58b6054c0230a4d54dfd4851c708dccc194fb83
BugaPunk <punksbuga@outlook.es>
2023-11-20 18:57:41 -0400

    [ADD] readme Proyecto finalizado

--- src/components/CountryItem.vue
+++ src/components/CountryItem.vue
@@ -30,7 +30,7 @@ const closePopup = () => {
                 v-if="country.coatOfArms.png"
                 :src="country.coatOfArms.png"
                 alt=""
-                style="width: 17px; height: 1px; display: inline"
+                style="width: 17px; height: 17px; display: inline"
             />
         </span>
         <div class="text-sm py-1">
@@ -99,45 +99,6 @@ const closePopup = () => {
                 </svg>
             </a>
         </div>
-    </div>
-    <button @click="openPopup">Abrir popup</button>
-    <div v-if="isModalVisible" class="bg-black bg-opacity-75">
-        <section
-            class="top-full overflow-hidden rounded-lg shadow-2xl"
-        >
-            <img
-                alt="Trainer"
-                src="https://images.unsplash.com/photo-1611510338559-2f463335092c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=928&q=80"
-                class="h-32 w-full object-cover md:h-50"
-            />
-
-            <div class="p-4 text-center sm:p-6 md:col-span-2 lg:p-8">
-                <p class="text-sm font-semibold uppercase tracking-widest">
-                    Run with the pack
-                </p>
-
-                <h2 class="mt-6 font-black uppercase">
-                    <span class="text-4xl font-black sm:text-5xl lg:text-6xl">
-                        Get 20% off
-                    </span>
-
-                    <span class="mt-2 block text-sm"
-                        >On your next order over $50</span
-                    >
-                </h2>
-
-                <a
-                    class="mt-8 inline-block w-full bg-black py-4 text-sm font-bold uppercase tracking-widest text-white"
-                    href=""
-                >
-                    Get Discount
-                </a>
-
-                <p class="mt-8 text-xs font-medium uppercase text-gray-400">
-                    Offer valid until 24th March, 2021 *
-                </p>
-            </div>
-        </section>
     </div>
 </template>
 <style scoped>
